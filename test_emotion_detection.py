11b9d2a60147dbe674d0574ef44ed069340ead77
Andy SIU <siuandyshc@gmail.com>
2025-01-21 09:11:17 -0500

    Amend Unit test

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -3,7 +3,7 @@ import unittest
 
 class TestEmotionDetector(unittest.TestCase):
     def test_emotion_detector(self):
-        #Test case 1
+        # Test case 1
         result_1 = emotion_detector('I am glad this happened')
         self.assertEqual(result_1['dominant_emotion'], 'joy')
 
@@ -14,5 +14,13 @@ class TestEmotionDetector(unittest.TestCase):
         # Test case 3
         result_3 = emotion_detector('I feel disgusted just hearing about this')
         self.assertEqual(result_3['dominant_emotion'], 'disgust')
+
+        # Test case 4
+        result_4 = emotion_detector('I am so sad about this')
+        self.assertEqual(result_4['dominant_emotion'], 'sadness')
+
+        # Test case 5
+        result_5 = emotion_detector('I am really afraid that this will happen')
+        self.assertEqual(result_5['dominant_emotion'], 'fear')
 
 unittest.main()
