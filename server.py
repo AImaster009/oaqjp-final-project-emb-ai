f0aa3bc14322c62df5c6a1736d864d04d6708df3
Andy Siu <siuandyshc@gmail.com>
2025-01-22 08:25:43 -0500

    change Port to 6000

--- server.py
+++ server.py
@@ -17,4 +17,4 @@ def emotionDetector(text_to_analyze):
     The dominant emotion is <strong>{result_emotion_dir['dominant_emotion']}</strong>.""")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=6000, debug=True)
