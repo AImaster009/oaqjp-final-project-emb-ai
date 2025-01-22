5f4b99894972baa046d0eea7d2844fd41317a30a
Andy Siu <siuandyshc@gmail.com>
2025-01-22 09:32:55 -0500

    Finish All tasks, YESgit status!

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,6 +5,16 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = {"raw_document": { "text": text_to_analyze }}
     response = requests.post(url, json = myobj, headers=header)
+    if response.status_code == 400:
+        resp = {
+            "anger": None, 
+            "disgust": None, 
+            "fear": None, 
+            "joy": None, 
+            "sadness": None, 
+            "dominant_emotion": None
+            }
+        return resp
     formatted_response = json.loads(response.text)
     dominant_emotion = formatted_response['emotionPredictions'][0]['emotion']
     biggest_value_emotion = max(dominant_emotion, key=dominant_emotion.get)
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+This module will send the var "textToAnalyze" to IBM AI API and return emotion
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,16 +9,25 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
+    """
+    This function renders the main page of the application.
+    """
     return render_template('index.html')
 
 @app.route('/<text_to_analyze>', methods=['GET'])
-def emotionDetector(text_to_analyze):
-    input_text_to_analyze = request.args.get('textToAnalyze')
-    result_emotion_dir = emotion_detector(input_text_to_analyze)
-    return (f"""For the given statement, the system response is 'anger': {result_emotion_dir['anger']}, 
-    'disgust': {result_emotion_dir['disgust']}, 'fear': {result_emotion_dir['fear']}, 
-    'joy': {result_emotion_dir['joy']} and 'sadness': {result_emotion_dir['sadness']}. 
-    The dominant emotion is <strong>{result_emotion_dir['dominant_emotion']}</strong>.""")
+def emotion_detector_name(text_to_analyze):
+    """
+    This function receives the text to analyze and runs emotion detection
+    """
+    text_to_analyze = request.args.get('textToAnalyze')
+    result_emotion_dir = emotion_detector(text_to_analyze)
+    if result_emotion_dir['dominant_emotion'] is None:
+        return "<strong>Invalid text! Please try again!</strong>"
+    return f"""For the given statement, the system response is 'anger':
+    {result_emotion_dir['anger']},'disgust': {result_emotion_dir['disgust']},
+    'fear': {result_emotion_dir['fear']},'joy': {result_emotion_dir['joy']} and 'sadness': 
+    {result_emotion_dir['sadness']}.The dominant emotion is 
+    <strong>{result_emotion_dir['dominant_emotion']}</strong>."""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=6000, debug=True)
